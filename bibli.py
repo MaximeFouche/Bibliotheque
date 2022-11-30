29f4987cb01f814c317aa67c06d390462995dd2c
MaximeFouche <114243800+MaximeFouche@users.noreply.github.com>
2022-11-30 17:13:32 +0100

    Create bibli.py

---
+++ bibli.py
@@ -0,0 +1,45 @@
+#!/bin/env python3
+from modules.Bibliotheque import Bibliotheque
+from modules.Bibliotheque import combiner_paths
+import sys
+import os
+import configparser
+import logging
+
+
+
+config = configparser.ConfigParser()
+with open("bibli.conf", "r") as config_file:
+    config.read_file(config_file)
+
+if __name__ == "__main__":
+    args = sys.argv
+    config_file_path ="" 
+    log_file = "log.log"
+
+    
+
+    if len(args) == 1:
+        print("usage: bibli.py init | pour initialiser la bibliothèque")
+        print("usage: bibli.py update | pour mettre à une bibliothèque existente")
+        print("options: -c 'config_file' | pour spécifier le fichier de configuration")
+    elif len(args) >= 2:
+        if "-c" in args:
+            pos = args.index("-c")
+            try:
+                with open(args[pos+1], "r") as config_file:
+                    config.read_file(config_file)
+                    log_file = config["DEFAULT"]["fichier_log"]
+            except:
+                raise Exception("Impossible d'ouvrir le fichier de configuration, le fichier existe t'il bien ?")
+        logging.basicConfig(filename=log_file, format='%(asctime)s %(message)s',encoding='utf-8', level=logging.DEBUG)
+
+        if args[-1] == "init":
+            print("Initialisation de la bibliothèque")
+            bibli = Bibliotheque("./livres/")
+            print("hey")
+            bibli.initialise()
+            print("fin")
+        elif args[-1] == "update":
+            bibli = Bibliotheque("./livres/")
+            bibli.update()
